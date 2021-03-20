c43a8044cb0b8e80bd24ac7c6c303c47af8ded59
ilyass <ilyass.ezzaouchi@gmail.com>
2021-03-20 18:34:32 +0100

    fix projects section

--- src/image/SocialMedia/SocialMedia.js
+++ src/image/SocialMedia/SocialMedia.js
@@ -11,29 +11,35 @@ import 'semantic-ui-css/semantic.min.css';
 import './SocialMedia.css';
 class SocialMedia extends Component {
     render() {
+        const socialMedias = [
+            {src1:WhiteFacebook,
+                src2: facebook,
+                link:"https://www.facebook.com/profile.php?id=100008194386790"},
+            {src1:WhiteInsta,
+                src2: insta,
+                link:"https://www.instagram.com/ilyass_ezzaouchi/"},
+            {src1:WhiteLinkedin,
+                src2: linkedin,
+                link:"https://www.linkedin.com/in/ilyass-ejjaouchi/"
+            },
+            {src1: githubWhite,
+                src2: github,
+                link:"https://www.linkedin.com/in/ilyass-ejjaouchi/"
+            }
+        ]
+        const listicons = socialMedias.map((icon) =>
+            <div>
+                <a href={icon.link} target="_blank">
+                    <img className="Socialicon" src={icon.src1} alt={icon.src1}
+                         onMouseOver={e => e.currentTarget.src = icon.src2}
+                         onMouseLeave={e => e.currentTarget.src = icon.src1}/>
+                </a><br/>
+            </div>
+        );
         return (
             <div className="six wide column socialMedia">
                 <div className="right floated">
-                    <a href="https://www.facebook.com/profile.php?id=100008194386790" target="_blank">
-                        <img className="Socialicon" src={WhiteFacebook} alt="whiteFacebook"
-                             onMouseOver={e => e.currentTarget.src = facebook}
-                             onMouseLeave={e => e.currentTarget.src = WhiteFacebook}/>
-                    </a><br/>
-                    <a href="https://www.instagram.com/ilyass_ezzaouchi/" target="_blank">
-                        <img className="Socialicon" src={WhiteInsta} alt="WhiteFacebook"
-                             onMouseOver={e => e.currentTarget.src =insta }
-                             onMouseLeave={e => e.currentTarget.src = WhiteInsta}/>
-                    </a><br/>
-                    <a href="https://www.linkedin.com/in/ilyass-ejjaouchi/" target="_blank">
-                        <img className="Socialicon" src={WhiteLinkedin} alt="WhiteLinkedin"
-                             onMouseOver={e => e.currentTarget.src =linkedin }
-                             onMouseLeave={e => e.currentTarget.src = WhiteLinkedin}/>
-                    </a><br/>
-                    <a href="https://github.com/ilyass-ejjaouchi" target="_blank">
-                        <img className="Socialicon" src={githubWhite} alt="WhiteLinkedin"
-                             onMouseOver={e => e.currentTarget.src =github }
-                             onMouseLeave={e => e.currentTarget.src = githubWhite}/>
-                    </a><br/>
+                    {listicons}
                 </div>
             </div>
         );
