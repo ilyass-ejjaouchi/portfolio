ae93ec065fc362332b458b80f38274476eb65ff5
Ilyass EJJAOUCHI <ilyass.ezzaouchi@gmail.com>
2021-09-15 00:25:54 +0200

    add email track contact

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -2,9 +2,38 @@ import React , {Component} from 'react';
 import 'semantic-ui-css/semantic.min.css';
 import './Form.css'
 import {withTranslation} from "react-i18next";
+import emailjs from 'emailjs-com';
 
 class Form extends Component {
-
+constructor() {
+    super();
+    this.state = {
+        firstName: '',
+        lastName: '',
+        message: '',
+        email: '',
+    }
+}
+    handleFirstNameChange = (e)=> {
+        this.setState({firstName: e.target.value});
+    }
+    handleLastNameChange = (e)=> {
+        this.setState({lastName: e.target.value});
+    }
+    handleMessageChange = (e)=> {
+        this.setState({message: e.target.value});
+    }
+    handleEmailChange = (e)=> {
+        this.setState({email: e.target.value});
+    }
+    submit = ()=> {
+        emailjs.send("service_qitv8og","template_9zmjdc3", this.state,"user_5HWFh3miaxgyWCJMlwGKJ")
+            .then(function(response) {
+                console.log('SUCCESS!', response.status, response.text);
+            }, function(error) {
+                console.log('FAILED...', error);
+            });
+    }
     render() {
         const {t} = this.props;
         return (
@@ -15,22 +44,31 @@ class Form extends Component {
                         <div className="two fields">
                             <div className="field">
                                 <label>{t('label.FirstName')}</label>
-                                <input placeholder={t('label.FirstName')} type="text"/>
+                                <input placeholder={t('label.FirstName')}
+                                       type="text"
+                                       onChange={this.handleFirstNameChange}/>
                             </div>
                             <div className="field">
                                 <label>{t('label.LastName')}</label>
-                                <input placeholder={t('label.LastName')} type="text"/>
+                                <input placeholder={t('label.LastName')}
+                                       type="text"
+                                        onChange={this.handleLastNameChange}/>
                             </div>
                         </div>
                         <div className="field">
                             <label>{t('label.Email')}</label>
-                            <input type="text" name="email" placeholder={t('label.Email')}/>
+                            <input type="text" name="email"
+                                   placeholder={t('label.Email')}
+                                   onChange={this.handleEmailChange}/>
                         </div>
                         <div className="field">
                             <label>{t('label.Message')}</label>
-                            <textarea rows="2" spellCheck="false" data-ms-editor="true"></textarea>
+                            <textarea rows="2" spellCheck="false"
+                                      data-ms-editor="true"
+                                      onChange={this.handleMessageChange}></textarea>
                         </div>
-                        <div className="ui button" type="submit">{t('label.Submit')}</div>
+                        <div className="ui button" type="submit"
+                            onClick={this.submit}>{t('label.Submit')}</div>
                     </form>
                 </div>
 
